c67ee75145301ae4d3f05a90629fe0c53bf72974
jjfarra@espol.edu.ec <joby.farra@nodel.co>
2023-10-30 14:30:29 -0500

    Upload file fixed and new changes

--- dashboard.py
+++ dashboard.py
@@ -48,12 +48,24 @@ if actual_file is not None:
     st.title("Programming Fundamentals Review".upper(), anchor=False)
 
     selected_student = None
-    with st.sidebar.container():
-        selected_student = st.selectbox("Choose a student:", dataframe["nombre_completo"])
-    student_data = dataframe[dataframe.nombre_completo == selected_student]
 
+    with st.sidebar.container():
+        selected_student = st.selectbox("Choose a student:", dataframe["nombre_completo"].unique())
+        st.markdown("""
+            <style>
+                .eczjsme4 {
+                    padding-top: 10px;
+                    padding-right: 10px;
+                    padding-bottom: 30px;
+                    padding-left: 10px;
+                }
+            </style>
+        """, unsafe_allow_html=True)
+    student_data = dataframe[dataframe.nombre_completo == selected_student]
+    student_data["estado"] = student_data.apply(lambda x: "AP" if((x.nota_teorico*0.7)+(x.nota_practico*0.3))>60 else "RP", axis=1)
+    student_data = student_data.sort_values(by="anio",ascending=False).reset_index(drop=True)
     st.markdown("""
-        <div font-size: 20px;>
+        <div font-size: 200px;>
             <span>STUDENT INFORMATION</span>
         </div>
     """, unsafe_allow_html=True)
@@ -65,6 +77,8 @@ if actual_file is not None:
             }
         </style>
         """, unsafe_allow_html=True)
+        years_period = [f"{str(row.anio)}-{row.periodo}" for idx,row in student_data.iterrows()]
+        tabs = st.tabs(years_period)
 
         st.write("---")
         id_student, full_name, id_career, id_course, time_taken = st.columns(5)
@@ -79,8 +93,8 @@ if actual_file is not None:
             st.subheader("**ID CARRERA**", anchor=False)
             st.write(f"{student_data['carrera'].values[0]}")
         with id_course:
-            st.subheader("**PARALELO ACTUAL**", anchor=False)
-            st.write(f"{student_data['paralelo_actual'].values[0]}")
+            st.subheader("**PARALELO**", anchor=False)
+            st.write(f"{student_data['paralelo'].values[0]}")
         with time_taken:
             st.subheader("**VEZ TOMADA**", anchor=False)
             st.write(f"{student_data['vez_tomada'].values[0]}")
@@ -92,6 +106,20 @@ if actual_file is not None:
                        }
                    </style>
                    """, unsafe_allow_html=True)
+        with st.container():
+            year,period, teacher, status = st.columns(4, gap="small")
+            with year:
+                st.subheader("**AÑO**", anchor=False)
+                st.write(f"{student_data['anio'].values[0]}")
+            with period:
+                st.subheader("**PERIODO**", anchor=False)
+                st.write(f"{student_data['periodo'].values[0]}")
+            with teacher:
+                st.subheader("**PROFESOR**", anchor=False)
+                st.write(f"{student_data['profesor'].values[0]}")
+            with status:
+                st.subheader("**ESTADO**", anchor=False)
+                st.write(f"{student_data['estado'].values[0]}")
         st.write("---")
 
     with st.container():
@@ -104,38 +132,46 @@ if actual_file is not None:
     st.write("---")
 
     with st.container():
-        lessons, workshops = st.columns(2, gap="small")
-
-        with lessons:
-            st.header("Lecciones", anchor=False)
-            l_1, l_2, l_3, l_4 = st.columns(4)
-            with l_1:
-                generate_gauge_chart('leccion_1', '# 1', 150, 350)
-            with l_2:
-                generate_gauge_chart('leccion_2', '# 2', 150, 350)
-            with l_3:
-                generate_gauge_chart('leccion_3', '# 3', 150, 350)
-            with l_4:
-                generate_gauge_chart('leccion_4', '# 4', 150, 350)
-        st.markdown("""<hr style="width: 3px; height: 300px; border: 0;
-         background-color: black;">""", unsafe_allow_html=True)
-
-        with workshops:
-            st.header("Talleres", anchor=False)
-            w_1, w_2, w_3, w_4 = st.columns(4)
-            with w_1:
-                generate_gauge_chart('taller_1', '# 1', 150, 350)
-            with w_2:
-                generate_gauge_chart('taller_2', '# 2', 150, 350)
-            with w_3:
-                generate_gauge_chart('taller_3', '# 3', 150, 350)
-            with w_4:
-                generate_gauge_chart('taller_4', '# 4', 150, 350)
+        st.header("Lecciones", anchor=False)
+        l_1, l_2, l_3, l_4 = st.columns(4)
+        with l_1:
+            generate_gauge_chart('leccion_1', '# 1', 200, 350)
+        with l_2:
+            generate_gauge_chart('leccion_2', '# 2', 200, 350)
+        with l_3:
+            generate_gauge_chart('leccion_3', '# 3', 200, 350)
+        with l_4:
+            generate_gauge_chart('leccion_4', '# 4', 200, 350)
+    st.write("---")
+    with st.container():
+        st.header("Talleres", anchor=False)
+        w_1, w_2, w_3, w_4 = st.columns(4)
+        with w_1:
+            generate_gauge_chart('taller_1', '# 1', 200, 350)
+        with w_2:
+            generate_gauge_chart('taller_2', '# 2', 200, 350)
+        with w_3:
+            generate_gauge_chart('taller_3', '# 3', 200, 350)
+        with w_4:
+            generate_gauge_chart('taller_4', '# 4', 200, 350)
+    st.write("---")
+    with st.container():
+        st.header("Examenes", anchor=False)
+        e_p,e_f,e_m = st.columns(3)
+        with e_p:
+            generate_gauge_chart('examen_parcial', '# 1', 200, 350)
+        with e_f:
+            generate_gauge_chart('examen_final', '# 2', 200, 350)
+        with e_m:
+            generate_gauge_chart('examen_mejoramiento', '# 3', 200, 350)
 
         st.markdown("""
            <style>
                .e1nzilvr1 {
                        text-align: center;
+           }
+           .e1vs0wn30{
+                align-items: center;
            }
             </style>
        """, unsafe_allow_html=True)
